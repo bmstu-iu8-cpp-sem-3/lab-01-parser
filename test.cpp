8a71ef5c04973681dc1631963a0cf96a78f0bc6a
cppguru <42873523+matsakiv@users.noreply.github.com>
2018-09-26 19:12:07 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -36,6 +36,50 @@ TEST(Json, LoadFromString) {
   EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
 }
 
+TEST(Json, NestedObjects) {
+  auto object = Json::parse(R"({"key":{"d":{"key":{}}}})");
+  auto key_object = std::any_cast<Json>(object["key"]);
+  auto d_object = std::any_cast<Json>(key_object["d"]);
+  ASSERT_NO_THROW(auto last_object = std::any_cast<Json>(d_object["key"]));
+}
+
+TEST(Json, NestedArrays) {
+  auto array = Json::parse(R"([[],["key"],[]])");
+  ASSERT_NO_THROW(auto array0 = std::any_cast<Json>(array[0]));
+  ASSERT_NO_THROW(auto array2 = std::any_cast<Json>(array[2]));
+
+  auto array1 = std::any_cast<Json>(array[1]);
+  EXPECT_EQ(std::any_cast<std::string>(array1[0]), "key");
+}
+
+TEST(Json, EntitiesCombination) {
+  auto object = Json::parse(R"({"key":[[[],"key"]]})");
+  auto array0 = std::any_cast<Json>(object["key"]);
+  auto array1 = std::any_cast<Json>(array0[0]);
+  ASSERT_NO_THROW(auto array2 = std::any_cast<Json>(array1[0]));
+  EXPECT_EQ(std::any_cast<std::string>(array1[1]), "key");
+}
+
+TEST(Json, SimpleObjects) {
+  Json object = Json::parse(R"({"k1":{},"k2":{"key":"value"},"k3":{}})");
+  ASSERT_NO_THROW(auto k1 = std::any_cast<Json>(object["k1"]));
+  ASSERT_NO_THROW(auto k3 = std::any_cast<Json>(object["k3"]));
+
+  auto k2 = std::any_cast<Json>(object["k2"]);
+  EXPECT_EQ(std::any_cast<std::string>(k2["key"]), "value");
+}
+
+TEST(Json, ObjectsAndArraysWithNumeric) {
+  auto object = Json::parse(R"({"k1":{},"k2":{"key": 40},"k3":[0.4]})");
+  ASSERT_NO_THROW(auto k1 = std::any_cast<Json>(object["k1"]));
+
+  auto k2 = std::any_cast<Json>(object["k2"]);
+  EXPECT_EQ(std::any_cast<double>(k2["key"]), 40);
+
+  auto k3 = std::any_cast<Json>(object["k3"]);
+  EXPECT_EQ(std::any_cast<double>(k3[0]), 0.4);
+}
+
 TEST(Json, NoThrow) {
   ASSERT_NO_THROW(Json(R"({"key":[[[]]]})"));
   ASSERT_NO_THROW(Json(R"([[],["key"],[]])"));

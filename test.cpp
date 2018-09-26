0c895626dfaf3bd217ae5663aa560c8a5936eb7e
cppguru <42873523+matsakiv@users.noreply.github.com>
2018-09-26 19:16:38 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -25,11 +25,11 @@ TEST(Json, LoadFromString) {
   Json object(json_data);
   EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
   EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
-  EXPECT_EQ(std::any_cast<int>(object["age"]), 25);
+  EXPECT_EQ(std::any_cast<double>(object["age"]), 25);
 
   auto marks = std::any_cast<Json>(object["marks"]);
-  EXPECT_EQ(std::any_cast<int>(marks[0]), 4);
-  EXPECT_EQ(std::any_cast<int>(marks[1]), 5);
+  EXPECT_EQ(std::any_cast<double>(marks[0]), 4);
+  EXPECT_EQ(std::any_cast<double>(marks[1]), 5);
 
   auto address = std::any_cast<Json>(object["address"]);
   EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");

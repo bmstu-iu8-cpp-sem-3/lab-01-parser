f540ca65b1f6a35c64f625ac89e0c8db5249f61e
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-09-24 07:43:39 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,39 @@
-// Copyright 2018 Your Name <your_email>
+#include <any>
+#include <string>
 
 #include <gtest/gtest.h>
+#include "json.hpp"
 
-#include <header.hpp>
+namespace {
+
+const char * const json_data = R"(
+{
+    "lastname" : "Ivanov",
+    "firstname" : "Ivan",
+    "age" : 25,
+    "islegal" : false,
+    "marks" : [
+    	4,5,5,5,2,3
+    ],
+    "address" : {
+    	"city" : "Moscow",
+        "street" : "Vozdvijenka"
+    }
+})";
+
+TEST(Json, LoadFromString) {
+  Json object(json_data);
+  EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
+  EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
+  EXPECT_EQ(std::any_cast<int>(object["age"]), 25);
+
+  auto marks = std::any_cast<Json>(object["marks"]);
+  EXPECT_EQ(std::any_cast<int>(marks[0]), 4);
+  EXPECT_EQ(std::any_cast<int>(marks[1]), 5);
+
+  auto address = std::any_cast<Json>(object["address"]);
+  EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
+  EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
+}
+
+}  // namespace

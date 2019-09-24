8392abfdbd544846db68e9ee71428ee487eb3043
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-09-24 07:46:55 +0300

    Create main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,38 @@
+#include "lib.hpp"
+#include <any>
+#include <vector>
+#include <map>
+
+
+using json =  nlohmann::json;
+nlohmann::json redistributionOfJson(nlohmann::json& input)
+{
+    nlohmann::json array = nlohmann::json::array();
+    nlohmann::json object = nlohmann::json::object();
+    for (size_t i = 0; i < input.size(); ++i)
+    {
+        size_t a = 0;
+        object["ticker"] = input[a][i];
+        ++a;
+        object["id"] = input[a][i];
+        ++a;
+        object["description"] = input[a][i];
+        array.push_back(object);
+    }
+    return array;
+}
+
+int main()
+{
+    const char * const json_data = R"(
+[
+["Si-9.15", "RTS-9.15", "GAZP-9.15"],
+[100024, 100027, 100050],
+["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
+]
+)";
+    nlohmann::json input;
+    input = nlohmann::json::parse(json_data);
+    nlohmann::json output  = redistributionOfJson(input);
+    return 0;
+}

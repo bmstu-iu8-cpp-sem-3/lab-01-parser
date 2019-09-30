c4b0423e178675b8efb9ac677ee2f8d2cff1a485
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-09-30 23:07:33 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,39 @@
-// Copyright 2018 Your Name <your_email>
+Copyright 2019 Your Name <ara07.99@icloud.com>
 
 #include <gtest/gtest.h>
 
-#include <header.hpp>
+#include "header.hpp"
+
+TEST(Json, ExampleJson)
+{
+    Json object = Json::parse(R"(
+        {
+            "lastname" : "Ivanov",
+            "firstname" : "Ivan",
+            "age" : 25,
+            "islegal" : false,
+            "marks" : [
+                4,5,5,5,2,3
+            ],
+            "address" : {
+                "city" : "Moscow",
+                "street" : "Vozdvijenka"
+            }
+        }
+    )");
+
+    EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
+    EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
+    EXPECT_EQ(std::any_cast<double>(object["age"]), 25);
+
+    Json &marks = *std::any_cast<Json *>(object["marks"]);
+    EXPECT_EQ(std::any_cast<double>(marks[0]), 4);
+    EXPECT_EQ(std::any_cast<double>(marks[1]), 5);
+
+    Json &address = *std::any_cast<Json *>(object["address"]);
+    EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
+    EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
+}
+
+
+

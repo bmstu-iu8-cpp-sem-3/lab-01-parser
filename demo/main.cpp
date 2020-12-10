b95188211a33f66abd0c076076e7fc1703d7833a
rusdevops <rusdevops@gmail.com>
2020-12-10 22:11:05 +0300

    added package with docs

---
+++ demo/main.cpp
@@ -0,0 +1,5 @@
+#include <header.hpp>
+
+int main() {
+
+}
---
+++ scripts/package.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
+CPACK_CONFIG_OPTS="-DCPACK_OUTPUT_FILE_PREFIX=$(pwd)/_artifacts -DCPACK_GENERATOR=DEB"
+CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Release -DCMAKE_BUILD_TYPE=Release -DBUILD_DOCS=ON"
+CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='$(pwd)/tools/polly/gcc-pic-cxx17.cmake'"
+CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS $CPACK_CONFIG_OPTS"
+cmake -H. -B_builds $CMAKE_OPTS
+cmake --build _builds --target docs
+cmake --build _builds --target package
+
+CPACK_CONFIG_OPTS="-DCPACK_OUTPUT_FILE_PREFIX=$(pwd)/_artifacts -DCPACK_GENERATOR=RPM"
+CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS $CPACK_CONFIG_OPTS"
+cmake -H. -B_builds $CMAKE_OPTS
+cmake --build _builds --target package
+

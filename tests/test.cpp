974377f5c3bef971503b7e2bdacd30b2ae0228d5
Max <vlamaster08@gmail.com>
2021-09-15 22:52:01 +0300

    added test

--- tests/test.cpp
+++ tests/test.cpp
@@ -60,51 +60,52 @@ TEST(ParseTest, TableTest){
     EXPECT_EQ(ss.str(), expect_string);
 }
 
-//TEST(Json_parser_test, parse_string) {
-//    std::string test_string =
-//            R"({
-//              "items": [
-//                {
-//                  "name": "Ivan",
-//                  "group": "IU1-11",
-//                  "avg": "4.2",
-//                  "debt": null
-//                },
-//                {
-//                  "name": "Pavel",
-//                  "group": 123,
-//                  "avg": 5,
-//                  "debt": "MatAn"
-//                },
-//                {
-//                  "name": "Borya",
-//                  "group": "IU2-12",
-//                  "avg": 4.66,
-//                  "debt": [
-//                    "Angem",
-//                    "Programming"
-//                  ]
-//                }
-//              ],
-//              "_meta": {
-//                "count": 3
-//              }
-//            })";
-//    auto students = ParseString(test_string);
-//    std::stringstream parsed;
-//    Print(students, parsed);
-//    std::string compare =
-//            R"(| name  | group  | avg      | debt    |
-//|-------|--------|----------|---------|
-//| Ivan  | IU1-11 | 4.2      | null    |
-//|-------|--------|----------|---------|
-//| Pavel | 123    | 5        | MatAn   |
-//|-------|--------|----------|---------|
-//| Borya | IU2-12 | 4.660000 | 2 items |
-//|-------|--------|----------|---------|)";
-//    std::cout << "Yes and?" << std::endl;
-//    EXPECT_EQ(compare,parsed.str());
-//}
+TEST(Json_parser_test, parse_string2) {
+    std::string test_string =
+            R"({
+              "items": [
+                {
+                  "name": "Ivan",
+                  "group": "IU1-11",
+                  "avg": "4.2",
+                  "debt": null
+                },
+                {
+                  "name": "Pavel",
+                  "group": 123,
+                  "avg": 5,
+                  "debt": "MatAn"
+                },
+                {
+                  "name": "Borya",
+                  "group": "IU2-12",
+                  "avg": 4.66,
+                  "debt": [
+                    "Angem",
+                    "Programming"
+                  ]
+                }
+              ],
+              "_meta": {
+                "count": 3
+              }
+            })";
+    auto students = ParseString(test_string);
+    std::stringstream parsed;
+    Print(students, parsed);
+    std::string compare =
+            R"(| name  | group  | avg      | debt    |
+|-------|--------|----------|---------|
+| Ivan  | IU1-11 | 4.2      | null    |
+|-------|--------|----------|---------|
+| Pavel | 123    | 5        | MatAn   |
+|-------|--------|----------|---------|
+| Borya | IU2-12 | 4.660000 | 2 items |
+|-------|--------|----------|---------|
+)";
+    std::cout << "Yes and?" << std::endl;
+    EXPECT_EQ(compare,parsed.str());
+}
 //
 //              "items": [
 //                {

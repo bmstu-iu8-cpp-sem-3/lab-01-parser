02979f98bf3894771ec1b32aa728c67e33be7ab3
Max <vlamaster08@gmail.com>
2021-09-15 22:13:51 +0300

    tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,35 +10,34 @@ TEST(Example, EmptyTest) {
 TEST(ParseTest, TableTest){
     std::string wrong_string =
             R"({
-<<<<<<< HEAD
-  "items": [
-    {
-      "name": "Jmishenko Valeriy",
-      "group": "9",
-      "avg": "4.25",
-      "debt": null
-    },
-    {
-      "name": "Vitaliy Tsal",
-      "group": 31,
-      "avg": 4,
-      "debt": "C++"
-    },
-    {
-      "name": "Evgeniy Ponasenkov",
-      "group": "IU8-31",
-      "avg": 3.33,
-      "debt": [
-        "C++",
-        "Linux",
-        "Network"
-      ]
-    }
-  ],
-  "_meta": {
-    "count": 3
-  }
-})";
+              "items": [
+                {
+                  "name": "Jmishenko Valeriy",
+                  "group": "9",
+                  "avg": "4.25",
+                  "debt": null
+                },
+                {
+                  "name": "Vitaliy Tsal",
+                  "group": 31,
+                  "avg": 4,
+                  "debt": "C++"
+                },
+                {
+                  "name": "Evgeniy Ponasenkov",
+                  "group": "IU8-31",
+                  "avg": 3.33,
+                  "debt": [
+                    "C++",
+                    "Linux",
+                    "Network"
+                  ]
+                }
+              ],
+              "_meta": {
+                "count": 3
+              }
+            })";
     auto students = ParseString(wrong_string);
 
     std::stringstream ss;
@@ -102,46 +101,46 @@ TEST(ParseTest, TableTest){
 //    std::cout << "Yes and?" << std::endl;
 //    EXPECT_EQ(compare,parsed.str());
 //}
-=======
-              "items": [
-                {
-                  "name": "Ivan",
-                  "group": "IU1-11",
-                  "avg": "4.2",
-                  "debt": null
-                },
-                {
-                  "name": "Pavel",
-                  "group": 123,
-                  "avg": 5,
-                  "debt": "MatAn"
-                },
-                {
-                  "name": "Borya",
-                  "group": "IU2-12",
-                  "avg": 4.66,
-                  "debt": [
-                    "Angem",
-                    "Programming"
-                  ]
-                }
-              ],
-              "_meta": {
-                "count": 3
-              }
-            })";
-    auto students = ParseString(test_string);
-    std::stringstream parsed;
-    Print(students, parsed);
-    std::string compare =
-            R"(| name  | group  | avg      | debt    |
-|-------|--------|----------|---------|
-| Ivan  | IU1-11 | 4.2      | null    |
-|-------|--------|----------|---------|
-| Pavel | 123    | 5        | MatAn   |
-|-------|--------|----------|---------|
-| Borya | IU2-12 | 4.660000 | 2 items |
-|-------|--------|----------|---------|)";
-    EXPECT_EQ(compare,parsed.str());
-}
->>>>>>> 4dd23c51f7e95cd66eb90d7b75a3751ac0ec5239
+//
+//              "items": [
+//                {
+//                  "name": "Ivan",
+//                  "group": "IU1-11",
+//                  "avg": "4.2",
+//                  "debt": null
+//                },
+//                {
+//                  "name": "Pavel",
+//                  "group": 123,
+//                  "avg": 5,
+//                  "debt": "MatAn"
+//                },
+//                {
+//                  "name": "Borya",
+//                  "group": "IU2-12",
+//                  "avg": 4.66,
+//                  "debt": [
+//                    "Angem",
+//                    "Programming"
+//                  ]
+//                }
+//              ],
+//              "_meta": {
+//                "count": 3
+//              }
+//            })";
+//    auto students = ParseString(test_string);
+//    std::stringstream parsed;
+//    Print(students, parsed);
+//    std::string compare =
+//            R"(| name  | group  | avg      | debt    |
+//|-------|--------|----------|---------|
+//| Ivan  | IU1-11 | 4.2      | null    |
+//|-------|--------|----------|---------|
+//| Pavel | 123    | 5        | MatAn   |
+//|-------|--------|----------|---------|
+//| Borya | IU2-12 | 4.660000 | 2 items |
+//|-------|--------|----------|---------|)";
+//    EXPECT_EQ(compare,parsed.str());
+//}
+//>>>>>>> 4dd23c51f7e95cd66eb90d7b75a3751ac0ec5239

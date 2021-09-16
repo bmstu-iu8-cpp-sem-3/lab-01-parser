feaf1e6016b531605a3e295496387a3caabc5d4a
Max <vlamaster08@gmail.com>
2021-09-16 21:00:53 +0300

    quality

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@ using json = nlohmann::json;
 
 int main(int argc, char* argv[]) {
     if (argc != 1) {
-        for (int i = 1; i <= argc; i++) {
+        for (int i = 1; i < argc; i++) {
             auto students = parse_json_file(argv[i]);
             Print(students, std::cout);
         }

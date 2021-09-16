e7ff3c088171f58d7406a97d6710ade205eb28e5
Max <vlamaster08@gmail.com>
2021-09-16 21:06:27 +0300

    quality

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,14 +4,17 @@
 
 using json = nlohmann::json;
 
-int main(int argc, char* argv[]) {
-    if (argc != 1) {
-        for (int i = 1; i < argc; i++) {
-            auto students = parse_json_file(argv[i]);
-            Print(students, std::cout);
-        }
-    } else {
-        throw std::invalid_argument("Expected a path for a *.json file");
-    }
+//int main(int argc, char* argv[]
+int main() {
+    auto students = parse_json_file("json_file.json");
+    Print(students, std::cout);
+//    if (argc != 1) {
+//        for (int i = 1; i < argc; i++) {
+//            auto students = parse_json_file(argv[i]);
+//            Print(students, std::cout);
+//        }
+//    } else {
+//        throw std::invalid_argument("Expected a path for a *.json file");
+//    }
     return 0;
 }

6d211ff29e995cf641a34b47239514bc4e47e86f
Max <vlamaster08@gmail.com>
2021-09-21 01:00:07 +0300

    for resourse files

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,7 +5,7 @@
 
 
 TEST(Json_parser_test, parse_right_file) {
-    auto students = parse_json_file("../RESOURSCES/json_file1.json");
+    auto students = parse_json_file("json_file1.json");
     std::stringstream parsed;
     Print(students, parsed);
     std::string compare =

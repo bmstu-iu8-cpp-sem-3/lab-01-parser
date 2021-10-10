14d724761833743d817e1f8b33fffd50ae542174
rusdevops <rusdevops@gmail.com>
2021-10-10 23:03:59 +0200

    updated test case

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,9 +1,11 @@
 // Copyright 2021 Your Name <your_email>
 
+#include <stdexcept>
+
 #include <gtest/gtest.h>
 
 #include <example.hpp>
 
 TEST(Example, EmptyTest) {
-    EXPECT_THROW(example());
+    EXPECT_THROW(example(), std::runtime_error);
 }
